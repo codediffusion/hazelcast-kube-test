235b93efb638e20ff7263cae81c42239d437336f
Rahul <codedistilled@gmail.com>
2020-12-19 12:29:56 +0000

    Added swagger ui

--- src/main/java/com/hazelcast/test/ApplicationBootstrap.java
+++ src/main/java/com/hazelcast/test/ApplicationBootstrap.java
@@ -1,4 +1,19 @@
 package com.hazelcast.test;
 
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Import;
+
+@SpringBootApplication
+@Import(SwaggerConfig.class)
 public class ApplicationBootstrap {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ApplicationBootstrap.class)
+                .web(WebApplicationType.SERVLET)
+                .headless(true)
+                .run(args);
+    }
+
 }
---
+++ src/main/java/com/hazelcast/test/SwaggerConfig.java
@@ -0,0 +1,67 @@
+package com.hazelcast.test;
+
+import com.fasterxml.classmate.TypeResolver;
+import com.google.common.base.Predicate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.context.request.async.DeferredResult;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.AlternateTypeRules;
+import springfox.documentation.service.Tag;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.lang.reflect.WildcardType;
+import java.time.LocalDate;
+
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Autowired
+    private TypeResolver typeResolver;
+
+    public Predicate<String> selector() {
+        return path -> (path.startsWith("/v1"));
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(selector())
+                .build()
+                .pathMapping("/")
+                .directModelSubstitute(LocalDate.class, String.class)
+                .genericModelSubstitutes(ResponseEntity.class)
+                .forCodeGeneration(true)
+                .alternateTypeRules(
+                        AlternateTypeRules.newRule(
+                                typeResolver.resolve(DeferredResult.class),
+                                typeResolver.resolve(ResponseEntity.class, WildcardType.class))
+                )
+                .useDefaultResponseMessages(false)
+                .tags(new Tag("HZ Kube test", "HZ Kube Test"));
+    }
+
+    @Bean
+    public UiConfiguration uiConfig() {
+        return new UiConfiguration(
+                "validationUrl",
+                "none",
+                "alpha",
+                "schema",
+                UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS,
+                false,
+                true,
+                60000L
+        );
+    }
+
+}
